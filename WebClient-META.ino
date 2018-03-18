d8b6f77735696b526a75cd9a3e7850e98908d51b
ishotjr <github@ishotjr.com>
2018-03-18 00:13:47 -0400

    :octocat: :tada: Initial commit

---
+++ WebClient-META.ino
@@ -0,0 +1,16 @@
+#include <Gamebuino-Meta.h>
+
+void setup(){
+
+  gb.begin();
+
+}
+
+void loop(){
+  while(!gb.update());
+  
+  gb.display.clear();
+  
+  gb.display.println("TODO: display web content!");
+  
+}
